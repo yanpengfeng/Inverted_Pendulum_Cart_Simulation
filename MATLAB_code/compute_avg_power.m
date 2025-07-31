bfa60466728b2bd7fa7d8518c778d03ff5f2f167
wuliao <3071224850@qq.com>
2025-07-31 21:50:17 +0800

    Update compute_avg_power.m

--- MATLAB_code/compute_avg_power.m
+++ MATLAB_code/compute_avg_power.m
@@ -5,8 +5,11 @@ current = zeros(N,1);
 power = zeros(N,1);
 
 for i = 1:N
+    %小车位移除以皮带轮盘半径等于转的圈数
     omega = x_dot(i) / p.r_pulley;
+    %输出减去电机速度常数乘以圈数然后再除以电机电阻得到电流
     current(i) = (Uout(i) - p.Kv * omega)/p.Rw;
+    %然后由电流乘以输出得到功率输出
     power(i) = current(i) * Uout(i);
 end
 
