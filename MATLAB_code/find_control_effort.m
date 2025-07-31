f22c587c631ccf606b74bea3cfd005a89d80c8d7
wuliao <3071224850@qq.com>
2025-07-31 23:05:34 +0800

    Update find_control_effort.m

--- MATLAB_code/find_control_effort.m
+++ MATLAB_code/find_control_effort.m
@@ -1,6 +1,7 @@
 function Uout = find_control_effort(t,X,desired_trajectory, p)
 % Compute the control effort for each controller 
-
+%获得输出参数更新
+%小车参考位置参数
 r_array = desired_trajectory(:,1); % reference signal of cart position
 
 t_prev = 0;
@@ -10,6 +11,7 @@ Uout = zeros(length(t),1);
 
     
 for i_data = 1:length(t)    
+    %取时间参数长度
     r = r_array(i_data);
     x = X(i_data, 1);
     theta = X(i_data,2);
@@ -19,6 +21,7 @@ for i_data = 1:length(t)
     [A,B,C,D] = compute_cart_pole_linear_system(X(i_data,:),r,p);
 
     if p.flag_ctrl == 0
+        %设计lqr控制器获得输出
         [K, Nbar, r_new] = lqr_controller(X(i_data,:)',r,p);
         Uout(i_data) = r_new * Nbar - K * X(i_data,:)'; 
         
@@ -32,18 +35,22 @@ for i_data = 1:length(t)
         Kd2 = p.Kd2;
 
 
+        %取得当前时刻参数和上一时刻参数
         t_cur = t(i_data);
         if i_data > 1  
             t_prev = t(i_data - 1);
         end
 
+        %更新角度和位置参数，通过误差计算得到
         integral_term_1_new = (t_cur - t_prev) * (r-x);
         integral_term_2_new = (t_cur- t_prev) * (-theta);
 
+        %更新角度和位移参数输出的控制器，双控制器设计
         u_pid = Kp1 * (r - x) + Kd1 * (-x_dot) + Ki1 * (integral_term_1_new + integral_term_1_prev);
         u_pid = u_pid + Kp2 * (-theta) + Kd2 * (-theta_dot) + Ki2 * (t_cur- t_prev) * (integral_term_2_new + integral_term_2_prev);
 
 
+        %更新参数，当前参数加旧参数
         integral_term_1_prev = integral_term_1_new + integral_term_1_prev;
 
         integral_term_2_prev = integral_term_2_new + integral_term_2_prev;
